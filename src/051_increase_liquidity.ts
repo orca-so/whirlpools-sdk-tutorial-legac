246ac37e621a6f5da0151ea16ac3bb6d4d4c07d5
boosik <bhs9194@nate.com>
2025-04-03 02:16:33 +0900

    feat: implement an example of decrease liquidity

--- src/051_increase_liquidity.ts
+++ src/051_increase_liquidity.ts
@@ -9,74 +9,70 @@ import secret from "../wallet.json";
 dotenv.config();
 
 async function main() {
-    try {
-        const rpc = createSolanaRpc(process.env.RPC_ENDPOINT_URL);
-        const signer = await setPayerFromBytes(new Uint8Array(secret));
-        await setRpc(process.env.RPC_ENDPOINT_URL);
-        await setWhirlpoolsConfig("solanaDevnet");
-        setPriorityFeeSetting({
-            type: "dynamic",
-            maxCapLamports: BigInt(5_000_000), // Max priority fee = 0.005 SOL
-        });
-        setJitoTipSetting({
-            type: "dynamic",
-        });
+    const rpc = createSolanaRpc(process.env.RPC_ENDPOINT_URL);
+    const signer = await setPayerFromBytes(new Uint8Array(secret));
+    await setRpc(process.env.RPC_ENDPOINT_URL);
+    await setWhirlpoolsConfig("solanaDevnet");
+    setPriorityFeeSetting({
+        type: "dynamic",
+        maxCapLamports: BigInt(5_000_000), // Max priority fee = 0.005 SOL
+    });
+    setJitoTipSetting({
+        type: "dynamic",
+    });
 
-        console.log('signer:', signer.address);
+    console.log('signer:', signer.address);
+
+    const devSAMO = {mint: address("Jd4M8bfJG3sAkd82RsGWyEXoaBXQP7njFzBwEaCTuDa"), decimals: 9};
+    const devUSDC = {mint: address("BRjpCHtyQLNCo8gqRUr8jtdAj5AjPYQaoqbvcZiHok1k"), decimals: 6};
     
-        const devSAMO = {mint: address("Jd4M8bfJG3sAkd82RsGWyEXoaBXQP7njFzBwEaCTuDa"), decimals: 9};
-        const devUSDC = {mint: address("BRjpCHtyQLNCo8gqRUr8jtdAj5AjPYQaoqbvcZiHok1k"), decimals: 6};
-        
-        const devUsdcAmount = BigInt(1_000_000); // 1 devUSDC
-        const slippage = 0.01; // 1%
-        const tickSpacing = 64;
+    const devUsdcAmount = BigInt(1_000_000); // 1 devUSDC
+    const slippage = 0.01; // 1%
+    const tickSpacing = 64;
+
+    const DEVNET_WHIRLPOOLS_CONFIG = address("FcrweFY1G9HJAHG5inkGB6pKg1HZ6x9UC2WioAfWrGkR");
+    const whirlpoolPda = await getWhirlpoolAddress(
+        DEVNET_WHIRLPOOLS_CONFIG,
+        devSAMO.mint,
+        devUSDC.mint,
+        tickSpacing,
+    );
+    console.log('whirlpoolPda:', whirlpoolPda);
+    const whirlpool = await fetchWhirlpool(rpc, whirlpoolPda[0]);
     
-        const DEVNET_WHIRLPOOLS_CONFIG = address("FcrweFY1G9HJAHG5inkGB6pKg1HZ6x9UC2WioAfWrGkR");
-        const whirlpoolPda = await getWhirlpoolAddress(
-            DEVNET_WHIRLPOOLS_CONFIG,
-            devSAMO.mint,
-            devUSDC.mint,
-            tickSpacing,
+    const positions = (await fetchPositionsForOwner(rpc, signer.address))
+        .filter(position => (position.data as Position).whirlpool === whirlpoolPda[0]);
+    console.log(positions);
+
+    if (positions.length > 0) {
+        // Only increase liquidity for the first position
+        const position: Position = positions[0].data as Position;
+        const calculatedQuote = increaseLiquidityQuoteB(
+            devUsdcAmount,
+            slippage,
+            whirlpool.data.sqrtPrice,
+            position.tickLowerIndex,
+            position.tickUpperIndex
         );
-        console.log('whirlpoolPda:', whirlpoolPda);
-        const whirlpool = await fetchWhirlpool(rpc, whirlpoolPda[0]);
-        
-        const positions = (await fetchPositionsForOwner(rpc, signer.address))
-            .filter(position => (position.data as Position).whirlpool === whirlpoolPda[0]);
-        console.log(positions);
-    
-        if (positions.length > 0) {
-            // Only increase liquidity for the first position
-            const position: Position = positions[0].data as Position;
-            const calculatedQuote = increaseLiquidityQuoteB(
-                devUsdcAmount,
-                slippage,
-                whirlpool.data.sqrtPrice,
-                position.tickLowerIndex,
-                position.tickUpperIndex
-            );
-            console.log("quote:", calculatedQuote);
-            console.log("devSAMO max input:", Number(calculatedQuote.tokenMaxA) / 10 ** devSAMO.decimals);
-            console.log("devUSDC max input:", Number(calculatedQuote.tokenMaxB) / 10 ** devUSDC.decimals);
- 
-            const { quote, instructions, callback: executeIncreaseLiquidity } = await increasePosLiquidity(
-                position.positionMint, 
-                {
-                    tokenB: calculatedQuote.tokenMaxB,
-                }, 
-                0.01
-            );
-            console.log("quote:", quote);
-            console.log("increaseLiquidityInstructions:", instructions);
+        console.log("quote:", calculatedQuote);
+        console.log("devSAMO max input:", Number(calculatedQuote.tokenMaxA) / 10 ** devSAMO.decimals);
+        console.log("devUSDC max input:", Number(calculatedQuote.tokenMaxB) / 10 ** devUSDC.decimals);
 
-            const signature = await executeIncreaseLiquidity();
-            console.log('signature:', signature);
-        } else {
-            console.log("No position in devSAMO/devUSDC pool");
-        }
-    } catch (e) {
-        console.error('error:', e);
+        const { quote, instructions, callback: executeIncreaseLiquidity } = await increasePosLiquidity(
+            position.positionMint, 
+            {
+                tokenB: calculatedQuote.tokenMaxB,
+            }, 
+            0.01
+        );
+        console.log("quote:", quote);
+        console.log("increaseLiquidityInstructions:", instructions);
+
+        const signature = await executeIncreaseLiquidity();
+        console.log('signature:', signature);
+    } else {
+        console.log("No position in devSAMO/devUSDC pool");
     }
 }
 
-main();
+main().catch(e => console.error(e));
